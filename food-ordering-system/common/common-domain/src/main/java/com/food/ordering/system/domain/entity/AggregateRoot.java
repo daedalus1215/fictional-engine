20c3e3813e5f5d05bb86352153c0be899cf11c90
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-05-13 10:07:02 -0400

    creating common domain module with base entity and aggregate root

---
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,10 @@
+package com.food.ordering.system.domain.entity;
+
+/**
+ * Simply a marker class, to distinguish the root objects from the other entities.
+ *
+ * @param <ID>
+ */
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+
+}
