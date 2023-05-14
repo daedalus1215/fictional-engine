9154b46e31a5cb2496c482c639663bb7cf075daa
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-05-14 18:56:28 -0400

    flush out OrderItem

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -1,2 +1,68 @@
-package com.food.ordering.system.order.service.domain.entity;public class OrderItem {
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
+
+public class OrderItem extends BaseEntity<OrderItemId> {
+    private OrderItemId orderItemId;
+    private final Product product;
+    private final int quantity;
+
+    private final Money price;
+
+    private final Money subTotal;
+
+    private OrderItem(Builder builder) {
+        super.setId(builder.orderItemId);
+        product = builder.product;
+        quantity = builder.quantity;
+        price = builder.price;
+        subTotal = builder.subTotal;
+    }
+
+    public OrderItemId getOrderItemId() {
+        return orderItemId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public Money getSubTotal() {
+        return subTotal;
+    }
+
+    public static final class Builder {
+        private OrderItemId orderItemId;
+        private final Product product;
+        private final int quantity;
+        private final Money price;
+        private final Money subTotal;
+
+        public Builder(Product product, int quantity, Money price, Money subTotal) {
+            this.product = product;
+            this.quantity = quantity;
+            this.price = price;
+            this.subTotal = subTotal;
+        }
+
+        public Builder id(OrderItemId val) {
+            orderItemId = val;
+            return this;
+        }
+
+        public OrderItem build() {
+            return new OrderItem(this);
+        }
+    }
 }
