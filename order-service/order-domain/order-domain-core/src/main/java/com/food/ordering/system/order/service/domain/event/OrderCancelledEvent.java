da82618fbc386c19214734b3ba4812486ad4913a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-05-21 12:50:25 -0400

    Create the OrderDomainService

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderCancelledEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderCancelledEvent.java
@@ -3,8 +3,6 @@ package com.food.ordering.system.order.service.domain.event;
 import com.food.ordering.system.domain.event.DomainEvent;
 import com.food.ordering.system.order.service.domain.entity.Order;
 
-import java.time.ZonedDateTime;
-
 public class OrderCancelledEvent implements DomainEvent<Order> {
 
 }
