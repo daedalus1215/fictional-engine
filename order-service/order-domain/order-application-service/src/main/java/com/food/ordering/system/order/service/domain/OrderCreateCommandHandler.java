6cb6470be624b677712dad2dc71dfc0627d1c5e3
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-05-30 20:11:01 -0400

    Publish with transactional listener

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -4,32 +4,37 @@ import com.food.ordering.system.order.service.domain.dto.create.CreateOrderComma
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
-import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
 public class OrderCreateCommandHandler {
     private final OrderDataMapper orderDataMapper;
     private final OrderCreateHelper orderCreateHelper;
-    private final OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher;
     private final ApplicationDomainEventPublisher applicationDomainEventPublisher;
 
     public OrderCreateCommandHandler(OrderDataMapper orderDataMapper,
                                      OrderCreateHelper orderCreateHelper,
-                                     OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher,
                                      ApplicationDomainEventPublisher applicationDomainEventPublisher) {
         this.orderDataMapper = orderDataMapper;
         this.orderCreateHelper = orderCreateHelper;
-        this.orderCreatedPaymentRequestMessagePublisher = orderCreatedPaymentRequestMessagePublisher;
         this.applicationDomainEventPublisher = applicationDomainEventPublisher;
     }
 
+    /**
+     * Using Transactional + TransactionEventListener in the OrderCreatedEventAppListener - The listener method will only
+     * be invoked when the Transaction has completed and committed.
+     *
+     * @param createOrderCommand
+     * @return
+     */
+    @Transactional
     public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
         OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
         log.info("Order is created with id: {}", orderCreatedEvent.getOrder().getOrderId());
-        orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
+        applicationDomainEventPublisher.publish(orderCreatedEvent);
         return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
     }
 }
