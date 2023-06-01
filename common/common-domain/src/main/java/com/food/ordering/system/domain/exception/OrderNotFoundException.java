11f09a60f9fd7dd222b2da0d3f83f159b0b3a688
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-05-31 21:47:05 -0400

    Create the OrderNotFoundException

--- common/common-domain/src/main/java/com/food/ordering/system/domain/exception/OrderNotFoundException.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/exception/OrderNotFoundException.java
@@ -1,2 +1,12 @@
-package com.food.ordering.system.domain.exception;public class OrderNotFoundException {
+package com.food.ordering.system.domain.exception;
+
+public class OrderNotFoundException extends DomainException{
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public OrderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
