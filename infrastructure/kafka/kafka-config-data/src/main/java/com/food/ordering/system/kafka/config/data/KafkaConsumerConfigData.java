2ac3bf0ff3a04f846efdd63baf65cf75ccd2c7c1
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-11 08:54:28 -0400

    implement kafka producer generic and model generic modules

--- infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConsumerConfigData.java
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConsumerConfigData.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "kafka-consumer-config")
