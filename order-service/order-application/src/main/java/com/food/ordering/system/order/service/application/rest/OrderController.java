c8c2bbf3246f05e7496739f4ee50978b85d1eb4b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-13 22:33:00 -0400

    create a Post endpoint for Orders Controller

--- order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -1,9 +1,31 @@
 package com.food.ordering.system.order.service.application.rest;
 
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/orders", produces = "application/vnd.api.v1+json")
 public class OrderController {
+    private final OrderApplicationService orderApplicationService;
+
+    public OrderController(OrderApplicationService orderApplicationService) {
+        this.orderApplicationService = orderApplicationService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
+        log.info("Creating order for customer: {} at restaurant: {}", createOrderCommand.getCustomerId());
+        return ResponseEntity.ok(orderApplicationService.createOrder(createOrderCommand));
+    }
+
+
 }
