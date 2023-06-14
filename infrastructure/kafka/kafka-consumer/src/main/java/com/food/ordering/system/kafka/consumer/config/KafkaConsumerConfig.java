57281a08695349c681ff640d283c8945f5d8f535
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-13 22:23:24 -0400

    implementing the kafka consumer

--- infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/config/KafkaConsumerConfig.java
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/config/KafkaConsumerConfig.java
@@ -4,8 +4,13 @@ import com.food.ordering.system.kafka.config.data.KafkaConfigData;
 import com.food.ordering.system.kafka.config.data.KafkaConsumerConfigData;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -24,19 +29,32 @@ public class KafkaConsumerConfig<K extends Serializable, V extends SpecificRecor
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers());
-
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaConsumerConfigData.getValueDeserializer());
-
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaConsumerConfigData.getValueDeserializer());
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaConsumerConfigData.getAutoOffsetReset());
         props.put(kafkaConfigData.getSchemaRegistryUrlKey(), kafkaConfigData.getSchemaRegistryUrl());
-//        //@TODO: Left off here
-//        props.put(ConsumerConfig.LINGER_MS_CONFIG, kafkaProducerConfigData.getLingerMs());
-//        props.put(ConsumerConfig.COMPRESSION_TYPE_CONFIG, kafkaProducerConfigData.getCompressionType());
-//        props.put(ConsumerConfig.ACKS_CONFIG, kafkaProducerConfigData.getAcks());
-//        props.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProducerConfigData.getRequestTimeoutMs());
-//        props.put(ConsumerConfig.RETRIES_CONFIG, kafkaProducerConfigData.getRetryCount());
+        props.put(kafkaConsumerConfigData.getSpecificAvroReaderKey(), kafkaConsumerConfigData.getSpecificAvroReader());
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, kafkaConsumerConfigData.getSessionTimeoutMs());
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, kafkaConsumerConfigData.getHeartbeatIntervalMs());
+        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG,
+                kafkaConsumerConfigData.getMaxPartitionFetchBytesDefault()
+                        * kafkaConsumerConfigData.getMaxPartitionFetchBytesBoostFactor());
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, kafkaConsumerConfigData.getMaxPollRecords());
         return props;
     }
 
+    @Bean
+    public ConsumerFactory<K, V> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<K, V>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<K, V> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setBatchListener(kafkaConsumerConfigData.getBatchListener());
+        factory.setConcurrency(kafkaConsumerConfigData.getConcurrencyLevel());
+        factory.setAutoStartup(kafkaConsumerConfigData.getAutoStartup());
+        factory.getContainerProperties().setPollTimeout(kafkaConsumerConfigData.getPollTimeoutMs());
+        return factory;
+    }
 }
