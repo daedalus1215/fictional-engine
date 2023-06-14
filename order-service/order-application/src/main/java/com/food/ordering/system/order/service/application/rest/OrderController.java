042aa20f99dd170c30d65a340d1e9afab08bc063
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-13 22:29:19 -0400

    Creare OrderController and use producer to indicate response type

---
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.order.service.application.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/orders", produces = "application/vnd.api.v1+json")
+public class OrderController {
+}
