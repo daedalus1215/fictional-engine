5b6f3b2830947d13de4b047c5c70f2c9defeb594
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-14 22:38:38 -0400

    add getOrderByTrackingId RH

--- order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -2,13 +2,14 @@ package com.food.ordering.system.order.service.application.rest;
 
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
 import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -27,5 +28,13 @@ public class OrderController {
         return ResponseEntity.ok(orderApplicationService.createOrder(createOrderCommand));
     }
 
-
+    @GetMapping("/{trackingId}")
+    public ResponseEntity<TrackOrderResponse> getOrderByTrackingId(@PathVariable UUID trackingId) {
+        final TrackOrderResponse trackOrderResponse = orderApplicationService.trackOrder(TrackOrderQuery
+                .builder()
+                .orderTrackingId(trackingId)
+                .build());
+        log.info("Returning order status with tracking id: {}", trackOrderResponse.getOrderTrackingId());
+        return ResponseEntity.ok(trackOrderResponse);
+    }
 }
