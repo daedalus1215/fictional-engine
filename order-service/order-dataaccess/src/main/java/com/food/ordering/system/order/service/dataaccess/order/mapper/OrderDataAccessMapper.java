a60ac1b108436db100fa7a2c5a4669c3826c8ee3
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-18 22:06:31 -0400

    Create OrderDataAccessMapper

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/mapper/OrderDataAccessMapper.java
@@ -0,0 +1,55 @@
+package com.food.ordering.system.order.service.dataaccess.order.mapper;
+
+import com.food.ordering.system.application.order.service.domain.entity.Order;
+import com.food.ordering.system.application.order.service.domain.entity.OrderItem;
+import com.food.ordering.system.application.order.service.domain.valueobject.StreetAddress;
+import com.food.ordering.system.order.service.dataaccess.order.entity.OrderAddressEntity;
+import com.food.ordering.system.order.service.dataaccess.order.entity.OrderEntity;
+import com.food.ordering.system.order.service.dataaccess.order.entity.OrderItemEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.food.ordering.system.application.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
+import static java.util.stream.Collectors.toList;
+
+@Component
+public class OrderDataAccessMapper {
+    public OrderEntity orderToOrderEntity(Order order) {
+        OrderEntity orderEntity = OrderEntity.builder()
+                .id(order.getId().getValue())
+                .customerId(order.getCustomerId().getValue())
+                .restaurantId(order.getRestaurantId().getValue())
+                .trackingId(order.getTrackingId().getValue())
+                .orderAddress(deliveryAddressToAddressEntity(order.getDeliveryAddress()))
+                .price(order.getPrice().getAmount())
+                .items(orderItemsToOrderItemEntities(order.getItems()))
+                .orderStatus(order.getOrderStatus())
+                .failureMessages(order.getFailureMessages() != null ?
+                        String.join(FAILURE_MESSAGE_DELIMITER, order.getFailureMessages()) : "")
+                .build();
+        orderEntity.getOrderAddress()
+    }
+
+    private List<OrderItemEntity> orderItemsToOrderItemEntities(List<OrderItem> items) {
+        return items.stream()
+                .map(orderItem -> OrderItemEntity.builder()
+                        .id(orderItem.getId().getValue())
+                        .productId(orderItem.getProduct().getId().getValue())
+                        .price(orderItem.getPrice().getAmount())
+                        .quantity(orderItem.getQuantity())
+                        .subTotal(orderItem.getSubTotal().getAmount())
+                        .build())
+                .collect(toList());
+
+    }
+
+    private OrderAddressEntity deliveryAddressToAddressEntity(StreetAddress deliveryAddress) {
+        return OrderAddressEntity.builder()
+                .id(deliveryAddress.getId())
+                .street(deliveryAddress.getStreet())
+                .postCode(deliveryAddress.getPostalCode())
+                .city(deliveryAddress.getCity())
+                .build();
+    }
+}
