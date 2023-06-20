1517242ef98c6f918b7ad9d75b3cc2728884a888
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-19 21:07:36 -0400

    Create Customer Aggregate Root

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/application/order/service/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/application/order/service/domain/entity/Customer.java
@@ -5,5 +5,10 @@ import com.food.ordering.system.application.domain.valueobject.CustomerId;
 
 public class Customer extends AggregateRoot<CustomerId> {
 
+    public Customer() {
+    }
 
+    public Customer(CustomerId customerId) {
+        super.setId(customerId);
+    }
 }
