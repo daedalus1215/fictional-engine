6aca7efd80fb84e142b992985125378c8b656901
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-19 22:31:50 -0400

    Fill in Customer Repostory Impl

--- order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/adapter/CustomerRepositoryImpl.java
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/adapter/CustomerRepositoryImpl.java
@@ -2,13 +2,26 @@ package com.food.ordering.system.order.service.dataaccess.customer.adapter;
 
 import com.food.ordering.system.application.order.service.domain.entity.Customer;
 import com.food.ordering.system.application.order.service.domain.ports.output.repository.CustomerRepository;
+import com.food.ordering.system.order.service.dataaccess.customer.mapper.CustomerDataAccessMapper;
+import com.food.ordering.system.order.service.dataaccess.customer.repository.CustomerJpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Component
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private final CustomerJpaRepository customerJpaRepository;
+    private final CustomerDataAccessMapper customerDataAccessMapper;
+
+    public CustomerRepositoryImpl(CustomerJpaRepository customerJpaRepository, CustomerDataAccessMapper customerDataAccessMapper) {
+        this.customerJpaRepository = customerJpaRepository;
+        this.customerDataAccessMapper = customerDataAccessMapper;
+    }
+
     @Override
     public Optional<Customer> findCustomer(UUID customerId) {
-        return Optional.empty();
+        return customer
     }
 }
