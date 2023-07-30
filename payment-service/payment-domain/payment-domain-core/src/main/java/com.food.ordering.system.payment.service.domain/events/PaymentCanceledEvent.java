8e20feb2967f27c8bafa44e9179037a3f7cb09c0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-29 20:58:39 -0400

    Payment Service > Payment Canceled Event

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com.food.ordering.system.payment.service.domain/events/PaymentCanceledEvent.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.payment.service.domain.events;
+
+import com.food.ordering.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class PaymentCanceledEvent extends PaymentEvent {
+    public PaymentCanceledEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+}
