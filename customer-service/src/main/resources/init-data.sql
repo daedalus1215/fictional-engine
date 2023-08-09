77b4f32b1eb29ecd68e60e569dac1b4082a7f225
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-08-09 16:49:09 -0400

    Wrap up Payment Service > Container

--- customer-service/src/main/resources/init-data.sql
+++ customer-service/src/main/resources/init-data.sql
@@ -1,5 +1,5 @@
 INSERT INTO customer.customers(id, username, first_name, last_name)
-VALUES('efe90d2f-f111-43d7-a07b-7c441609c92f', 'user_1', 'First', 'User')
+VALUES('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user_1', 'First', 'User');
 
 INSERT INTO customer.customers(id, username, first_name, last_name)
-VALUES('f285a29b-3f45-4d45-a63c-a10bc443b046', 'user_1', 'First', 'User')
+VALUES('f285a29b-3f45-4d45-a63c-a10bc443b046', 'user_2', 'Second', 'User');
