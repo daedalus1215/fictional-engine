956483b492a295147be847f19af9c227a0ce906c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-08-18 15:42:09 -0400

    comments

--- infrastructure/saga/src/main/java/com/food/ordering/system/saga/SagaStep.java
+++ infrastructure/saga/src/main/java/com/food/ordering/system/saga/SagaStep.java
@@ -5,5 +5,4 @@ import com.food.ordering.system.domain.event.DomainEvent;
 public interface SagaStep<T, S extends DomainEvent, U extends DomainEvent> {
     S process(T data);
     U rollback(T data);
-
 }
